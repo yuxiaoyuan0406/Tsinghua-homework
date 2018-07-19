4cb19eb54c95b70535fe819046fc3c8d354d62ec
DennisYu <973971990@qq.com>
2018-07-19 10:12:06 +0800

    consts changed, make stepper to move 30mm

--- motors/Stepper/main.cpp
+++ motors/Stepper/main.cpp
@@ -1,8 +1,16 @@
+//#include "stm32f10x.h"                  // Device header
 #include "mbed.h"
 
 Ticker ticker_step;
 DigitalOut step(PB_0), dir(PB_1), en(PC_13);
 volatile int remain;
+
+double targetDistance = 30.0;										//目标距离（毫米）
+double pitch = 3.6;															//螺距（毫米）
+double circle = targetDistance / pitch;					//圈数=目标距离/螺距
+double stepWidth = 1.8;													//步距角
+double stepNumberPerCircle = 360.0 / stepWidth;	//单圈步数=一圈/步距角
+
 
 void toggle_step() { //产生STEP脉冲信号
     if(remain == 0)
@@ -21,10 +29,10 @@ int main() {
     
     while (true) {
         dir = 0;
-        remain = 3200;
+        remain = stepNumberPerCircle * circle;
         wait(1.5);
         dir = 1;
-        remain = 3200;
+        remain = stepNumberPerCircle * circle;
         wait(1.5);
     }
 }
