91f673816ca368a189486a2fa8d19defde846e9c
DennisYu <973971990@qq.com>
2018-07-19 14:54:21 +0800

    stepper move 30mm

--- motors/Stepper/main.cpp
+++ motors/Stepper/main.cpp
@@ -6,9 +6,9 @@ DigitalOut step(PB_0), dir(PB_1), en(PC_13);
 volatile int remain;
 
 double targetDistance = 30.0;										//目标距离（毫米）
-double pitch = 3.6;															//螺距（毫米）
+double pitch = 3.5;															//螺距（毫米）
 double circle = targetDistance / pitch;					//圈数=目标距离/螺距
-double stepWidth = 1.8;													//步距角
+double stepWidth = 0.13;												//步距角
 double stepNumberPerCircle = 360.0 / stepWidth;	//单圈步数=一圈/步距角
 
 
@@ -24,15 +24,19 @@ void toggle_step() { //产生STEP脉冲信号
 }
 
 int main() {
-    ticker_step.attach(&toggle_step, 0.0002);
+    ticker_step.attach(&toggle_step, 0.0003);
     en = 0; //Enable stepper driver
     
     while (true) {
         dir = 0;
         remain = stepNumberPerCircle * circle;
-        wait(1.5);
+//        wait(1.5);
+				while(remain != 0)
+					;
         dir = 1;
         remain = stepNumberPerCircle * circle;
-        wait(1.5);
+//        wait(1.5);
+				while(remain != 0)
+					;
     }
 }
