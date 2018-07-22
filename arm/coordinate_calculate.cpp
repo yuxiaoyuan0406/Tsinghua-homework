f82d3872d15a9be3e1c18240e65b17a606bc5ba0
DennisYu <yuxiaoyuan0406@hotmail.com>
2018-07-22 20:33:01 +0800

    add a new folder:arm

---
+++ arm/coordinate_calculate.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    using namespace std;
+    double alpha = 0.0, beta = 0.0;
+    const double length[2] = {100.0, 100.0};
+    double x = 0, y = 0;
+    cout << "input coordinate (x, y)" << endl;
+    cin >> x >> y;
+    return 0;
+}
